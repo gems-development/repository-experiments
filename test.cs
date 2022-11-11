40bed0f3aeea2e954e1371397933f3d817e6dc8f
Дмитрий Долгушин <ddolgushin@yandex.ru>
2022-11-11 09:47:29 +0600

    + Init

---
+++ test.cs
@@ -0,0 +1,40 @@
+public class Student
+{
+	public string Name { get; }
+	public float Score { get; }
+	
+	public Student(string name, float score)
+	{
+		Name = name;
+		Score = score;
+	}
+}
+
+static class EnumerableExtensions
+{
+	public static int Count<T>(this IEnumerable<T> collection, Predicate<T> predicate)
+	{
+		var counter = 0;
+		
+		foreach (var item in collection)
+		{
+			if (predicate(item))
+				counter++;
+		}
+		
+		return counter;
+	}
+}
+
+void Main()
+{
+	var students = new []
+	{
+	  new Student(score: 4, name: "Иванов"),
+	  new Student(name: "Петров", score: 3),
+	  new Student(name: "Сидоров", score: 3.5f)
+	};
+	
+	Console.WriteLine(students.Count(student => student.Score >= 3.5));
+	Console.WriteLine(new[] { 1, 2, 3 }.Count(o => o > 2));
+}
